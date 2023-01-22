8389ecc65623d23f6a61e7b0c6b74ca4f4cfa74e
Yash Srivastav <yashsrivastav8115@gmail.com>
2023-01-22 12:37:45 +0530

    Day 5

---
+++ Day 5/Write a program to identify if the number is even or odd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int number;
+	cout<<"Enter Number to Check Odd or Even: ";
+	cin>>number;
+	if(number%2==0) cout<<"Even";
+	else cout<<"Odd";
+	
+	return 0;
+}
